650227d15381a96dd17d14bcafe212675b7cc966
JimmypageLedz <1714371762@qq.com>
2026-05-20 14:16:28 +0800

    前端优化：昇腾设备自动切换grafana dashboard

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -213,8 +213,11 @@ function switchGrafanaDevice() {
     const val = document.getElementById("custom-device-selector").value;
     if (!val) return;
     const ip = val.split(":")[0];
-    document.getElementById("grafana-frame").src = `http://10.144.144.2:3000/d/ad9hqhg/b9a97b3?orgId=1&from=now-6h&to=now&timezone=browser&refresh=auto&kiosk&var-device=${encodeURIComponent(ip)}`;
+    const isAscend = val.includes(":9500");
+    const dashboardId = isAscend ? "addfqnr/e0d73c7" : "ad9hqhg/b9a97b3";
+    document.getElementById("grafana-frame").src = `http://10.144.144.2:3000/d/${dashboardId}?orgId=1&from=now-6h&to=now&timezone=browser&refresh=auto&kiosk&var-device=${encodeURIComponent(ip)}`;
 }
+//新增判断是否是昇腾，若是昇腾，则展示grafana中昇腾专属dashborad，若不是，则展示用户监控大屏dashboard
 
 function switchView(viewId, navElement) {
     document.querySelectorAll('.view-section').forEach(el => el.classList.remove('active'));
